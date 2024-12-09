7545ccdb2d4882e16a0a792dc32ab929770c17c2
jayoadonis <jayo.adonisraphael@gmail.com>
2024-12-09 15:54:16 +0800

    [chore: 0.0.0] .|. Add "ASAP" partial routes

--- frontend/src/main/learn/web/frontend/simple_survey_form/App.tsx
+++ frontend/src/main/learn/web/frontend/simple_survey_form/App.tsx
@@ -1,15 +1,21 @@
+import * as ReactRouterDom from "react-router-dom"
+import SurveyApp from '@learn/web/frontend/simple_survey_form/SurveyApp.tsx'
+import "@resource/css/global-twcss.css";
 
-// import DontApplyTailwindCssBase from "@learn/web/frontend/simple_survey_form/util/DontApplyTailwindCSSBase"
 
-// import "@resource/css/dont-apply-twcss-base.css";
-
-function App() {
-
+function App(): React.ReactElement {
   return (
     <>
-    {/* <DontApplyTailwindCssBase> */}
-      <h1 className="text-red-500 text-[5rem]">asdf adfa Hi there...</h1>
-    {/* </DontApplyTailwindCssBase> */}
+    <ReactRouterDom.BrowserRouter>
+      <ReactRouterDom.Routes>
+        {/* <ReactRouterDom.Route path="/" element={<ChristmasLayout/>}> */}
+          <ReactRouterDom.Route index element={<SurveyApp/>}/>
+          <ReactRouterDom.Route path="/admin/login" element={<h1 className={`text-[2rem] p-4 text-lime-600`}>login, to be continue...</h1>}/>
+          <ReactRouterDom.Route path="/admin/partial-dashboard" element={<h1 className={`text-[2rem] p-4 text-lime-600`}>dashboard, to be continue...</h1>}/>
+          <ReactRouterDom.Route path="*" element={<h1 className={`text-[2rem] p-4 text-red-500`}>Page not found</h1>}/>
+        {/* </ReactRouterDom.Route> */}
+      </ReactRouterDom.Routes>
+    </ReactRouterDom.BrowserRouter>
     </>
   )
 }
--- frontend/src/main/learn/web/frontend/simple_survey_form/index.tsx
+++ frontend/src/main/learn/web/frontend/simple_survey_form/index.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import "@resource/css/global-twcss.css";
 
-import SurveyApp from '@learn/web/frontend/simple_survey_form/SurveyApp.tsx'
+
+import App from './App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <SurveyApp />
+    <App/>
   </StrictMode>,
 )
